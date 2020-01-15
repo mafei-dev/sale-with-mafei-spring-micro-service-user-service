7705404e7426e658b8d875251047aee83c04d414
mafei <mafei.dev@gmail.com>
2020-01-15 22:44:32 +0530

    Initial commit

---
+++ src/main/java/com/user/SaleWithMafeiSpringMicroServiceUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleWithMafeiSpringMicroServiceUserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaleWithMafeiSpringMicroServiceUserServiceApplication.class, args);
+    }
+
+}
