ae00ad3477917a5a1dc6584d61e5c029f89a88dc
mafei <mafei.dev@gmail.com>
2020-01-16 14:46:40 +0530

    properties

--- src/main/java/com/user/SaleWithMafeiSpringMicroServiceUserServiceApplication.java
+++ src/main/java/com/user/SaleWithMafeiSpringMicroServiceUserServiceApplication.java
@@ -3,7 +3,7 @@ package com.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SaleWithMafeiSpringMicroServiceUserServiceApplication {
 
     public static void main(String[] args) {
