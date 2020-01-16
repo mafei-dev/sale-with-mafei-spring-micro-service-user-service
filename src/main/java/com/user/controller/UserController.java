58b9dfa133ca6b327d23c4e34e359bf436f70229
mafei <mafei.dev@gmail.com>
2020-01-16 15:02:41 +0530

    properties

---
+++ src/main/java/com/user/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.user.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    public ResponseEntity<?> index() {
+        return new ResponseEntity<>("hi", HttpStatus.OK);
+    }
+}
